967b9cd9b80fce5326a4dba78f4a943bbadd7c1e
Dustin Chapman <dustin.r.chapman@gmail.com>
2014-05-03 21:37:53 -0400

    start of questions class

---
+++ questions.py
@@ -0,0 +1,26 @@
+import json
+
+
+class Question(object):
+
+    def __init__(self):
+        self._question = {}
+        self._question['type'] = self.__class__.__name__
+        self._question['stem'] = ''
+
+    def __str__(self):
+        return self._question['stem']
+
+    def dump(self):
+        return json.dumps(self._question)
+
+    def load(self, s):
+        self._question = json.loads(s)
+
+
+class MultipleChoice(Question):
+
+    def __init__(self):
+        super(MultipleChoice, self).__init__()
+        self._question['keys'] = []
+        self._question['distractors'] = []
