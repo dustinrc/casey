4231116463f3935ea3b5ec55eaef4a2ac0b9cb99
Dustin Chapman <dustin.r.chapman@gmail.com>
2014-05-09 16:14:46 -0400

    knowledge pools are loadable

--- knowledge.py
+++ knowledge.py
@@ -2,14 +2,17 @@ import json
 
 
 def create_knowledge_kind(kind, attrs):
-    k = type(kind, (Knowledge,), attrs)
+    k = type(str(kind), (Knowledge,), attrs)
     return k
 
 
 class KnowledgePool(object):
 
-    def __init__(self, kind):
-        self._kind = kind.__name__
+    def __init__(self, kind=None):
+        try:
+            self._kind = kind.__name__
+        except AttributeError:
+            self._kind = kind
         self._pool = []
 
     def dump(self):
@@ -18,6 +21,16 @@ class KnowledgePool(object):
             'pool': [x._data for x in self._pool],
         }
         return json.dumps(obj, indent=4)
+
+    def load(self, s):
+        # TODO: make this method not so ugly
+        obj = json.loads(s)
+        self._kind = obj['kind']
+        self._pool = []
+        for x in obj['pool']:
+            k = create_knowledge_kind(self._kind, {})
+            k._data = x
+            self.add(k)
 
     def add(self, knowledge):
         self._pool.append(knowledge)
